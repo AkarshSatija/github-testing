34c0aba71aa95172bd36e6ee8c45e39f5520b292
Akarsh <akarsh.satija@gmail.com>
2016-04-24 15:14:16 +0530

    Adding scripts

---
+++ scripts/try.sh
@@ -0,0 +1,59 @@
+d="2016-03-28 15:14:16 +0530"
+
+export GIT_AUTHOR_DATE=$d
+export GIT_COMMITTER_DATE=$d
+
+touch $d
+git add .; git commit -m $d;
+
+
+
+d="2016-03-29 15:14:16 +0530"
+
+export GIT_AUTHOR_DATE=$d
+export GIT_COMMITTER_DATE=$d
+
+touch $d
+git add .; git commit -m $d;
+
+
+
+d="2016-04-03 15:14:16 +0530"
+
+export GIT_AUTHOR_DATE=$d
+export GIT_COMMITTER_DATE=$d
+
+touch $d
+git add .; git commit -m $d;
+
+
+
+d="2016-04-10 15:14:16 +0530"
+
+export GIT_AUTHOR_DATE=$d
+export GIT_COMMITTER_DATE=$d
+
+touch $d
+git add .; git commit -m $d;
+
+
+
+d="2016-04-17 15:14:16 +0530"
+
+export GIT_AUTHOR_DATE=$d
+export GIT_COMMITTER_DATE=$d
+
+touch $d
+git add .; git commit -m $d;
+
+
+
+d="2016-04-24 15:14:16 +0530"
+
+export GIT_AUTHOR_DATE=$d
+export GIT_COMMITTER_DATE=$d
+
+touch $d
+git add .; git commit -m $d;
+
+
